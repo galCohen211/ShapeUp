73e9a704b121f27df0e21e65233527465ff2f0f1
Yoni Katz <yehonatan.katz@biocatch.com>
2024-12-19 18:40:11 +0200

    add logout test

--- src/tests/controllers/auth.test.ts
+++ src/tests/controllers/auth.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import fs from "fs";
 import path from "path";
+import jwt from "jsonwebtoken";
 
 import app from "../../server";
 import User from "../../models/user-model";
@@ -145,4 +146,72 @@ describe("Auth Endpoints", () => {
       expect(response.body.errors).toBeDefined();
     });
   });
+
+  describe("POST /logout", () => {
+
+    if (!process.env.JWT_SECRET) {
+      return { message: "Missing auth configuration" };
+    }
+
+    const token = jwt.sign(
+      { id: "123", type: "user" },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRATION }
+    );
+
+    const mockUser = {
+      _id: new mongoose.Types.ObjectId(),
+      email: "johndoe123@gmail.com",
+      password: "$2b$12$kiwSU0JHcdsDVJLOxDD2AekohHwS5RVU8E5wZerlnFE7/Jibvr10W", // bcrypt for 12345
+      firstName: "John",
+      lastName: "Doe",
+      address: "First Street",
+      type: "USER",
+      avatarUrl: "http://example.com/avatar.jpg",
+      refreshTokens: [token],
+      save: jest.fn().mockResolvedValue(true),
+    };
+
+    beforeEach(() => {
+      User.findOne = jest.fn().mockResolvedValue(mockUser);
+    });
+
+    it("should logout a user and return 200 on successful logout", async () => {
+
+      const response = await request(app)
+        .post("/users/logout")
+        .send({
+          refreshToken: token
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe("Logged out successfully");
+      expect(mockUser.refreshTokens).toHaveLength(0);
+      expect(mockUser.save).toHaveBeenCalled();
+    });
+
+    it("should return 400 on logout because notInListToken is not on refreshTokens list", async () => {
+
+      if (!process.env.JWT_SECRET) {
+        return { message: "Missing auth configuration" };
+      }
+
+      const notInListToken = jwt.sign(
+        { id: "123", type: "user" },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRATION }
+      );
+
+      const response = await request(app)
+        .post("/users/logout")
+        .send({
+          refreshToken: notInListToken
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBe("Invalid refresh token");
+      expect(mockUser.save).toHaveBeenCalled();
+    });
+  });
+
 });
