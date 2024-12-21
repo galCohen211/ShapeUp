8862369c7896a9b9956eb6bf7b2fefe006b3edfe
Gal Cohen <galcohen211@gmail.com>
2024-12-21 14:33:57 +0200

    Add Review

---
+++ src/routes/review-route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { body, query, param } from "express-validator";
+import reviewController from "../controllers/review-controller";
+import verifyToken from "../middleware/verifyToken";
+import { IUserType } from "../models/user-model";
+
+const router = express.Router();
+
+// Add a new review
+router.post("/",verifyToken([IUserType.USER]), reviewController.addReview);
+
+// Update a review
+// router.put("/:reviewId",verifyToken([IUserType.USER]), reviewController.updateReview);
+
+// Get all reviews
+// router.get("/", reviewController.getAllReviews);
+
+
+
+export default router;
