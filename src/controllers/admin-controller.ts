aba166a08a3d0eb9d6b936fba162c56269dcc3e4
Gal Cohen <galcohen211@gmail.com>
2025-04-04 20:26:47 +0300

    Feat: Admin page

--- src/controllers/admin-controller.ts
+++ src/controllers/admin-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import User, { IUserType } from "../models/user-model";
+import User, { IGymOwnerStatus, IUserType } from "../models/user-model";
 import Gym from "../models/gym-model";
 
 class AdminController {
@@ -30,6 +30,60 @@ class AdminController {
       });
     } catch (err) {
       res.status(500).json({ message: "Internal server error", error: err });
+    }
+  }
+
+  static async updateGymOwnerStatus(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const { gymOwnerId } = req.params;
+      const { status } = req.body;
+
+      // Validate the provided status
+      if (!Object.values(IGymOwnerStatus).includes(status)) {
+        res.status(400).json({ error: "Invalid gym owner status." });
+      }
+
+      // Find the user by ID
+      const user = await User.findById(gymOwnerId);
+
+      if (!user) {
+        res.status(404).json({ error: "User not found." });
+      } else {
+        // Check if the user is a gym owner
+        if (user.role !== IUserType.GYM_OWNER) {
+          res.status(400).json({ error: "User is not a gym owner." });
+        }
+
+        // Update the gym owner status
+        user.gymOwnerStatus = status;
+        await user.save();
+
+        res.status(200).json({
+          message: "Gym owner status updated successfully.",
+          status: user.gymOwnerStatus,
+        });
+      }
+    } catch (error) {
+      console.error("Error updating gym owner status:", error);
+      res.status(500).json({ error: "Internal server error." });
+    }
+  }
+
+  static async getGymOwnersStatus(_req: Request, res: Response): Promise<void> {
+    try {
+      // Find all gym owners and select the required fields
+      const gymOwners = await User.find(
+        { role: IUserType.GYM_OWNER },
+        "firstName lastName email city gymOwnerLicenseImage gymOwnerStatus"
+      );
+
+      // Return the list of gym owners with statuses
+      res.status(200).json(gymOwners);
+    } catch (error) {
+      console.error("Error fetching gym owners:", error);
     }
   }
 }
