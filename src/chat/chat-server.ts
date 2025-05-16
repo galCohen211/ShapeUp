d5c8927a0ede3d476871a1e72481b21cb62a97d2
Yoni Katz <yehonatan.katz@biocatch.com>
2025-05-16 15:27:39 +0300

    1

--- src/chat/chat-server.ts
+++ src/chat/chat-server.ts
@@ -8,30 +8,32 @@ const usersSocket: Record<string, Socket> = {};
 export function initChat(server: SocketIOServer): void {
   server.on("connection", (socket: Socket) => {
     socket.on("add_user", (userId: ObjectId) => {
-      if (userId != null) {
+      if(userId != null)
+      {
         usersSocket[userId.toString()] = socket;
       }
     });
 
     socket.on("remove_user", (userId: ObjectId) => {
-      if (userId != null) {
-        delete usersSocket[userId.toString()];
-        console.log('User Id - ' + userId + ' was disconnected from the chat');
-      }
+      if(userId != null)
+        {
+      delete usersSocket[userId.toString()];
+      console.log('User Id - ' + userId + ' was disconnected from the chat');
+        }
     });
 
     socket.on("communicate", async (userId1: ObjectId, userId2: ObjectId, gymName: string, text: string) => {
       try {
         await createChatBetweenUsers([userId1, userId2], gymName);
-
+    
         const newMessage = {
           sender: userId1,
           text: text,
           timestamp: new Date()
         };
-
+    
         await AddMessageToChat(userId1, userId2, gymName, newMessage as IMessage);
-
+    
         server.emit("message", newMessage);
 
       } catch (err) {
@@ -40,9 +42,9 @@ export function initChat(server: SocketIOServer): void {
     });
 
     socket.on("get_users_chat", async (userId1: ObjectId, userId2: ObjectId, gymName: string, callback) => {
-      try {
+      try {        
         const chatHistory = await getMessagesBetweenTwoUsers([userId1, userId2], gymName);
-
+    
         if (chatHistory) {
           callback({ messages: chatHistory.messages });
         } else {
@@ -52,11 +54,11 @@ export function initChat(server: SocketIOServer): void {
         callback({ messages: [] });
       }
     });
-
+    
     socket.on("get_gym_chats", async (ownerId: ObjectId, gymName: string, callback) => {
-      try {
+      try {    
         const chatUsers = await getGymChats(ownerId, gymName);
-
+    
         callback(chatUsers);
       } catch (error) {
         console.error("Error fetching gym chats:", error);
@@ -65,9 +67,9 @@ export function initChat(server: SocketIOServer): void {
     });
 
     socket.on("update_gym_name", async (ownerId: ObjectId, oldGymName: string, newGymName: string, callback) => {
-      try {
+      try {    
         const updatedChats = await updateGymName(ownerId, oldGymName, newGymName);
-
+    
         if (updatedChats > 0) {
           callback({ success: true, updatedChats });
         } else {
@@ -78,9 +80,10 @@ export function initChat(server: SocketIOServer): void {
         callback({ success: false, message: "Internal server error." });
       }
     });
-
+    
 
     socket.on("disconnect", () => {
+      console.log(`The user was disconnected`);
     });
   });
 }
